2ff27e148a82b7bd85438c7b00764aa7435bb577
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-13 17:35:53 +0100

    Adding dummy content to SidebarView

--- src/AnhNhan/ModHub/Views/Page/SidebarView.php
+++ src/AnhNhan/ModHub/Views/Page/SidebarView.php
@@ -3,6 +3,7 @@ namespace AnhNhan\ModHub\Views\Page;
 
 use AnhNhan\ModHub;
 use AnhNhan\ModHub\Views\AbstractView;
+use AnhNhan\ModHub\Views\Objects;
 
 /**
  * @author Anh Nhan Nguyen <anhnhan@outlook.com>
@@ -12,8 +13,34 @@ class SidebarView extends AbstractView
     public function render()
     {
         $menu_nav = ModHub\ht("div")
-            ->addClass("menu-nav")
-            ->setContent(ModHub\ht("p", "Content goes here"));
+            ->addClass("menu-nav");
+
+        $menu_nav->appendContent(ModHub\ht("h2", "Upcoming events"));
+
+		$listing = new Objects\Listing;
+		$menu_nav->appendContent($listing);
+
+		$listing->addObject(
+		    id(new Objects\Object)
+		        ->setHeadline("Swag Throwaway Thursday")
+		        ->addAttribute("27th June 2014")
+		        ->addAttribute("324 participants")
+		);
+
+		$listing->addObject(
+		    id(new Objects\Object)
+		        ->setHeadline("Community AmA")
+		        ->addAttribute("29th June 2014")
+		        ->addAttribute("985 participants")
+		);
+
+		$listing->addObject(
+		    id(new Objects\Object)
+		        ->setHeadline("SotP 3.2")
+		        ->addAttribute("Release")
+		        ->addAttribute("4th July 2014")
+		        ->addAttribute("985 participants")
+		);
 
         return $menu_nav;
     }
