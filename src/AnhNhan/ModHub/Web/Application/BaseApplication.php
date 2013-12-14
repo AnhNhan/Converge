b595a24a4ca1d05f2a9ea57a7c03c8869ff6ef03
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-14 03:21:55 +0100

    Adding base application classes

---
+++ src/AnhNhan/ModHub/Web/Application/BaseApplication.php
@@ -0,0 +1,56 @@
+<?php
+namespace AnhNhan\ModHub\Web\Application;
+
+use YamwLibs\Libs\Http\Request;
+use YamwLibs\Libs\Routing\Route;
+
+/**
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+abstract class BaseApplication
+{
+    /**
+     * Empty constructor
+     */
+    final public function __construct()
+    {
+        // Empty
+    }
+
+    abstract public function getHumanReadableName();
+    abstract public function getInternalName();
+
+    abstract public function getRoutes();
+
+    abstract public function routeToController(Request $request)
+    {
+        //
+    }
+
+    protected function generateRoutesFromYaml($file)
+    {
+        $yaml = \Symfony\Component\Yaml\Yaml::parse($file);
+        $routes = $yaml["routes"];
+
+        $routeObjects = array();
+        $ii = 0;
+        foreach ($routes as $blob) {
+            $route = new Route("route-$file-$ii", $blob["pattern"], $this);
+
+            $reqs = idx($blob, "requirements");
+            $params = idx($blob, "parameters");
+
+            if ($reqs) {
+                $route->setRequirements($reqs);
+            }
+            if ($params) {
+                $route->setParameters($params);
+            }
+
+            $routeObjects[] = $route;
+            $ii++;
+        }
+
+        return $routeObjects;
+    }
+}
---
+++ src/AnhNhan/ModHub/Web/Application/BaseApplicationController.php
@@ -0,0 +1,41 @@
+<?php
+namespace AnhNhan\ModHub\Web\Application;
+
+use YamwLibs\Libs\Http\Request;
+
+/**
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+abstract class BaseApplicationController
+{
+    /**
+     * @var BaseApplication
+     */
+    private $app;
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    final public function __construct(BaseApplication $app)
+    {
+        $this->app = $app;
+    }
+
+    final public function app()
+    {
+        return $this->app;
+    }
+
+    final public function request()
+    {
+        return $this->request();
+    }
+
+    final public function setRequest(Request $request)
+    {
+        $this->request = $request;
+        return $this;
+    }
+}
