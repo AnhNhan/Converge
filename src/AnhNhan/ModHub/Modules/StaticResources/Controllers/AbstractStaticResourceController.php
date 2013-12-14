065a5c2c42de45467fda63c48bf6e39090d359bf
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-14 12:01:30 +0100

    Adding static resource controller

---
+++ src/AnhNhan/ModHub/Modules/StaticResources/Controllers/AbstractStaticResourceController.php
@@ -0,0 +1,12 @@
+<?php
+namespace AnhNhan\ModHub\Modules\StaticResources\Controllers;
+
+use AnhNhan\ModHub\Web\Application\BaseApplicationController;
+
+/**
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+abstract class AbstractStaticResourceController extends BaseApplicationController
+{
+
+}
