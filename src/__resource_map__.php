2ef7264edb3435a7eb04b6b9dfe785bf899d09aa
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-18 23:28:20 +0100

    Adding back header background when on mobile

--- src/__resource_map__.php
+++ src/__resource_map__.php
@@ -54,8 +54,8 @@ return array(
     "layout-core" => array(
       "name" => "layout-core",
       "path" => "layout/core.less",
-      "hash" => "e08092ed",
-      "time" => "4d7367ad",
+      "hash" => "49a0b4a7",
+      "time" => "901d594c",
     ),
     "layout-form" => array(
       "name" => "layout-form",
@@ -118,13 +118,13 @@ return array(
     "core-pck" => array(
       "name" => "core-pck",
       "type" => "css",
-      "hash" => "0ef6757c",
+      "hash" => "fbcb8afe",
       "contents" => array(
         "external-bootstrap-responsive-utilities" => "37915821",
         "external-bootstrap-sprites" => "7f8d1ffb",
         "external-html5-boilerplate-main" => "93f2241c",
         "layout-boilerplate" => "bda7009e",
-        "layout-core" => "e08092ed",
+        "layout-core" => "49a0b4a7",
         "layout-form" => "148e0b57",
         "layout-grid" => "5cd5c19d",
         "layout-buttons" => "f57b3b3b",
