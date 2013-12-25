3cb2b283c639d78fd7d9c0d6bf3abdf36fc103fd
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-25 22:37:29 +0100

    Adding UserAuthenticationProvider

--- src/AnhNhan/ModHub/Modules/User/Providers/UserAuthenticationProvider.php
+++ src/AnhNhan/ModHub/Modules/User/Providers/UserAuthenticationProvider.php
@@ -9,8 +9,11 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Security\Core\Authentication\Provider\UserAuthenticationProvider as BaseUserAuthenticationProvider;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 
 /**
  * @author Anh Nhan Nguyen <anhnhan@outlook.com>
@@ -22,21 +25,35 @@ class UserAuthenticationProvider extends BaseUserAuthenticationProvider
      */
     private $userRepository;
 
+    /**
+     * @var \Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface
+     */
+    private $encoder;
+
     const USER_ENTITY_TYPE_NAME = "AnhNhan\ModHub\Modules\User\Storage\User";
 
     public function setUserApplication(UserApplication $userApp)
     {
         $this->setEntityManager($userApp->getEntityManager());
+        return $this;
     }
 
     public function setEntityManager(EntityManager $em)
     {
         $this->userRepository = $em->getRepository(self::USER_ENTITY_TYPE_NAME);
+        return $this;
     }
 
     public function setUserRepository(EntityRepository $repo)
     {
         $this->userRepository = $repo;
+        return $this;
+    }
+
+    public function setPasswordEncoder(PasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+        return $this;
     }
 
     protected function retrieveUser($username, UsernamePasswordToken $token)
@@ -47,11 +64,17 @@ class UserAuthenticationProvider extends BaseUserAuthenticationProvider
 
         $user = $this->userRepository->findOneBy(array("username" => $username));
 
+        if ($user === null) {
+            throw new UsernameNotFoundException("A user with the name '{$username}' could not be found!");
+        }
+
         return $user;
     }
 
     protected function checkAuthentication(UserInterface $user, UsernamePasswordToken $token)
     {
-        // TODO: Check password here
+        if (!$this->encoder->isPasswordValid($user->getPassword(), $token->getCredentials(), $user->getSalt())) {
+            throw new BadCredentialsException("Passwords do not match!");
+        }
     }
 }
---
+++ tests/unit_user/Providers/UserAuthenticationProviderTest.php
@@ -0,0 +1,123 @@
+<?php
+namespace Providers;
+
+use Codeception\Util\Stub;
+use AnhNhan\ModHub\Modules\User\Providers\UserAuthenticationProvider;
+use AnhNhan\ModHub\Modules\User\Storage\User;
+
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+class UserAuthenticationProviderTest extends \Codeception\TestCase\Test
+{
+   /**
+    * @var \CodeGuy
+    */
+    protected $codeGuy;
+
+    /**
+     * @var UserAuthenticationProvider
+     */
+    private $authProvider;
+
+    /**
+     * @var UserChecker
+     */
+    private $userChecker;
+
+    /**
+     * @var EntityRepository
+     */
+    private $repository;
+
+    private $token;
+
+    const PROVIDER_KEY = "providerKey";
+
+    protected function _before()
+    {
+        $this->repository   = $this->getMock('Doctrine\ORM\EntityRepository', array(), array(), '', false);
+        $this->userChecker  = $this->getMock('Symfony\Component\Security\Core\User\UserChecker');
+        $this->authProvider = new UserAuthenticationProvider($this->userChecker, self::PROVIDER_KEY, false);
+        $this->token        = new UsernamePasswordToken("foo", "bar", self::PROVIDER_KEY);
+
+        $this->authProvider->setUserRepository($this->repository);
+    }
+
+    protected function _after()
+    {
+        $this->authProvider = null;
+        $this->userChecker  = null;
+        $this->repository   = null;
+        $this->token        = null;
+    }
+
+    /**
+     * @expectedException Symfony\Component\Security\Core\Exception\UsernameNotFoundException
+     */
+    public function testAuthenticationFailsForNonExistingUser()
+    {
+        $this->repository
+            ->expects($this->once())
+            ->method("findOneBy")
+            ->will($this->returnValue(null));
+
+        $this->authProvider->authenticate($this->token);
+
+        $this->fail();
+    }
+
+    public function testASuccessfulLoginAttempt()
+    {
+        $user = $this->getMock('AnhNhan\ModHub\Modules\User\Storage\User', array(), array(), '', false);
+        // Assuming plain text password
+        $user->expects($this->once())->method("getPassword")->will($this->returnValue("bar"));
+        $user->expects($this->once())->method("getRoles")->will($this->returnValue(array("ROLE_USER")));
+
+        $this->repository
+            ->expects($this->once())
+            ->method("findOneBy")
+            ->with(array("username" => "foo"))
+            ->will($this->returnValue($user));
+
+        $passwordEncoder = $this->getMock('Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder');
+        $passwordEncoder->expects($this->once())
+                        ->method("isPasswordValid")
+                        ->with("bar", "bar", null)
+                        ->will($this->returnValue(true));
+        $this->authProvider->setPasswordEncoder($passwordEncoder);
+
+        $this->assertEquals(false, $this->token->isAuthenticated());
+        $newToken = $this->authProvider->authenticate($this->token);
+
+        $this->assertEquals(true, $newToken->isAuthenticated());
+    }
+
+    /**
+     * @expectedException Symfony\Component\Security\Core\Exception\BadCredentialsException
+     */
+    public function testLoginFailsWithWrongPassword()
+    {
+        $user = $this->getMock('AnhNhan\ModHub\Modules\User\Storage\User', array(), array(), '', false);
+        // Assuming plain text password
+        $user->expects($this->once())->method("getPassword")->will($this->returnValue("blurp"));
+
+        $this->repository
+            ->expects($this->once())
+            ->method("findOneBy")
+            ->with(array("username" => "foo"))
+            ->will($this->returnValue($user));
+
+        $passwordEncoder = $this->getMock('Symfony\Component\Security\Core\Encoder\PlaintextPasswordEncoder');
+        $passwordEncoder->expects($this->once())
+                        ->method("isPasswordValid")
+                        ->with("blurp", "bar", null) // Wrong password
+                        ->will($this->returnValue(false));
+        $this->authProvider->setPasswordEncoder($passwordEncoder);
+
+        $this->assertEquals(false, $this->token->isAuthenticated());
+        $newToken = $this->authProvider->authenticate($this->token);
+
+        $this->fail();
+    }
+
+}
