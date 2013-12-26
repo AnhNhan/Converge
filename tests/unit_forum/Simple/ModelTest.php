9c86081cbba32c4dab7386904f0c461a64ee83b4
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-26 18:24:31 +0100

    Adding test to add tags to Simple\ModelTest

--- tests/unit_forum/Simple/ModelTest.php
+++ tests/unit_forum/Simple/ModelTest.php
@@ -4,6 +4,9 @@ use Codeception\Util\Stub;
 use AnhNhan\ModHub\Modules\Forum\Storage\Discussion;
 use AnhNhan\ModHub\Modules\Forum\Storage\DiscussionTag;
 
+use AnhNhan\ModHub\Modules\Tag\TagApplication;
+use AnhNhan\ModHub\Modules\Tag\Storage\Tag;
+
 class ModelTest extends \Codeception\TestCase\Test
 {
     const ENTITY_NAME_DISCUSSION = 'AnhNhan\ModHub\Modules\Forum\Storage\Discussion';
@@ -25,6 +28,7 @@ class ModelTest extends \Codeception\TestCase\Test
 
     protected function _after()
     {
+        $this->forumGuy->getEntityManager()->clear();
     }
 
     public function testNewlyCreatedDiscussionsDontHaveTags()
@@ -40,6 +44,40 @@ class ModelTest extends \Codeception\TestCase\Test
         $discussion = $this->forumGuy->getRepository(self::ENTITY_NAME_DISCUSSION)->findOneBy(array("label" => "foo"));
         $this->assertEquals("foo", $discussion->label());
         $this->assertSame(0, $discussion->tags()->count());
+    }
+
+    public function testAddTagToDiscussion()
+    {
+        // Repeat previous test, create discussion without tags
+        $this->testNewlyCreatedDiscussionsDontHaveTags();
+
+        $entityManager = $this->forumGuy->getEntityManager();
+        $entityManager->clear();
+
+        $repository = $this->forumGuy->getRepository(self::ENTITY_NAME_DISCUSSION);
+        $discussion = $repository->findOneBy(array("label" => "foo"));
+
+        $tagApp = new TagApplication;
+        $tagEm  = $tagApp->getEntityManager();
+
+        $tag1 = new Tag(\Filesystem::readRandomCharacters(4), "green", null, -1);
+        $tag2 = new Tag(\Filesystem::readRandomCharacters(4), "blue", null, 1);
+        $tagEm->persist($tag1);
+        $tagEm->persist($tag2);
+        $tagEm->flush();
+
+        $disqTag1 = new DiscussionTag($discussion, $tag1);
+        $disqTag2 = new DiscussionTag($discussion, $tag2);
+        $entityManager->persist($disqTag1);
+        $entityManager->persist($disqTag2);
+        $entityManager->flush();
+        $entityManager->clear();
+
+        $this->forumGuy->canSeeInDatabase("DiscussionTag", array("t_id" => $tag1->uid()));
+        $this->forumGuy->canSeeInDatabase("DiscussionTag", array("t_id" => $tag2->uid()));
+
+        $discussion = $repository->findOneBy(array("label" => "foo"));
+        $this->assertEquals(2, $discussion->tags()->count());
     }
 
 }
