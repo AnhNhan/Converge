ee7daef67d4fb0300f8360d90ebf3d1c19177123
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-26 23:10:06 +0100

    Adding first forum acceptance test

---
+++ tests/acceptance_forum/Forms/SimpleCreateDiscussionCept.php
@@ -0,0 +1,29 @@
+<?php
+$I = new ForumWebGuy($scenario);
+$I->wantTo('simply create a new discussion');
+$I->lookForwardTo('am able to take part in the never ending discussion machine without having to feed the admin trolls');
+
+$I->am('some random user (hopefully logged in)');
+$I->amOnPage('/disq/');
+
+$label = "Some random discussion";
+$text  = "Blurby text";
+$I->dontSee($label);
+$I->click("Create new discussion");
+
+$I->canSeeInCurrentUrl("/disq/create");
+$I->see("new discussion");
+$I->fillField("label", $label);
+$I->fillField("text", $text);
+$I->click("Hasta la vista"); // Warning! May be changed!
+
+// Will be changed soon
+$I->see("Successfully inserted discussion");
+$I->click("Link");
+
+$I->see($label);
+$I->see($text);
+
+// Will be changed
+$I->click("Disq listing");
+$I->see($label);
