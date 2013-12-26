ff89f0d14f95f759a2cd9f41707f4a03128957f8
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-26 04:41:30 +0100

    More concise test output for tests/acceptance_tag/Forms/CreateNewTagCept.php

--- tests/acceptance_tag/Forms/CreateNewTagCept.php
+++ tests/acceptance_tag/Forms/CreateNewTagCept.php
@@ -1,6 +1,8 @@
 <?php
 $I = new TagAcceptanceGuy($scenario);
 $I->wantTo('create a new tag');
+$I->am('some random user (currently anonymous)');
+$I->amGoingTo('use the form to create a new tag');
 
 $label = "Foo";
 
@@ -9,6 +11,7 @@ $I->dontSee($label);
 
 $I->click('Create new tag!');
 $I->canSeeInCurrentUrl('create');
+$I->see('Create new tag');
 $I->submitForm('#tag-creation', array('label' => $label));
 
 $I->amOnPage('/tag/');
