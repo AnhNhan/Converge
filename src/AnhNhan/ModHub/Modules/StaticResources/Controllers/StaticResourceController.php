c6df5fc55131a5764c6415f0d67b4f73703fb21c
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-31 01:27:33 +0100

    Adding cache-ability to static resource serving

--- src/AnhNhan/ModHub/Modules/StaticResources/Controllers/StaticResourceController.php
+++ src/AnhNhan/ModHub/Modules/StaticResources/Controllers/StaticResourceController.php
@@ -3,6 +3,9 @@ namespace AnhNhan\ModHub\Modules\StaticResources\Controllers;
 
 use AnhNhan\ModHub;
 use AnhNhan\ModHub\Web\Application\RawHttpPayload;
+use YamwLibs\Libs\Assertions\BasicAssertions as BA;
+
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Anh Nhan Nguyen <anhnhan@outlook.com>
@@ -16,17 +19,42 @@ final class StaticResourceController extends AbstractStaticResourceController
         $name = $request->request->get("name");
         $rsrc_hash = $request->request->get("rsrc-hash");
 
-        $payload = new RawHttpPayload();
+        BA::assertIsEnum($type, array("css", "js"));
+
+        $resMap = include ModHub\path("__resource_map__.php");
+
+        if (!$resource = idx($resMap[$type], $name)) {
+            // Could be a pack file
+            $resource = idx($resMap["pck"], $name);
+        }
+
+        if (!$resource) {
+            throw new \Exception("Resource '{$name}' does not exist!");
+        }
 
         if ($type == "css") {
-            $payload->setHttpHeader("Content-Type", "text/css");
+            $contentType = "text/css";
         } else if ($type == "js") {
-            $payload->setHttpHeader("Content-Type", "application/javascript");
+            $contentType = "application/javascript";
+        }
+
+        $maxAge = 60 * 60 * 24 * 360 * 2;
+        $response = Response::create()
+            ->setCache(array(
+                "etag" => $resource["hash"],
+                "public" => true,
+                "max_age" => $maxAge,
+                "s_maxage" => $maxAge,
+            ))
+        ;
+        $response->headers->set("Content-Type", $contentType);
+
+        if ($notModified = $response->isNotModified($request)) {
+            return $response;
         }
 
         $contents = file_get_contents(ModHub\get_root_super() . "/cache/" . $name);
-        $payload->setPayloadContents($contents);
-
-        return $payload;
+        $response->setContent($contents);
+        return $response;
     }
 }
