c28c3615db2cbde33cd315a819b1e0bae3d1a31a
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-12-31 03:35:22 +0100

    Better date format for discussion listing

--- src/AnhNhan/ModHub/Modules/Forum/Controllers/DiscussionListingController.php
+++ src/AnhNhan/ModHub/Modules/Forum/Controllers/DiscussionListingController.php
@@ -48,7 +48,7 @@ final class DiscussionListingController extends AbstractForumController
             }
 
             $object->addAttribute($discussion->authorId());
-            $object->addAttribute($discussion->lastActivity()->format(DATE_RFC822));
+            $object->addAttribute($discussion->lastActivity()->format("D, d M 'y"));
 
             $listing->addObject($object);
         }
