e93fcff50dd7d906cd64891edf56d428e2dd8138
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-02 23:47:52 +0100

    Making Console/ConsoleCommand.php container-aware

--- src/AnhNhan/ModHub/Console/ConsoleCommand.php
+++ src/AnhNhan/ModHub/Console/ConsoleCommand.php
@@ -2,6 +2,7 @@
 namespace AnhNhan\ModHub\Console;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Used by SymbolLoader to trace all console commands contained in this application.
@@ -10,4 +11,28 @@ use Symfony\Component\Console\Command\Command;
  */
 abstract class ConsoleCommand extends Command
 {
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    final public function setContainer(ContainerInterface $container)
+    {
+        $this->container = $container;
+        return $this;
+    }
+
+    /**
+     * @return object|null The requested service or null if it does not exist (we
+     *                     ignore exceptions)
+     */
+    final public function getService($service)
+    {
+        return $this->container->get($service, ContainerInterface::NULL_ON_INVALID_REFERENCE);
+    }
+
+    final public function getServiceParameter($parameter)
+    {
+        return $this->container->getParameter($parameter);
+    }
 }
