7db6d6e46c221ad7db7ffc3ff0ea137342c6548d
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-02 23:52:49 +0100

    Removing default userplate

--- src/AnhNhan/ModHub/Views/Page/HeaderView.php
+++ src/AnhNhan/ModHub/Views/Page/HeaderView.php
@@ -13,13 +13,11 @@ class HeaderView extends AbstractView
 {
     public function render()
     {
-        $userPlate = new UserPlateView(new User("nyan-nyan", "An Nyan", "pw", "se"));
-        $userPlate->setAlignment(UserPlateView::IMAGE_ALIGN_RIGHT);
         $header_content = ModHub\ht("div")
             ->addClass("header-content")
-            ->appendContent(ModHub\ht("div", $userPlate)->addClass(""))
             ->appendContent(ModHub\ht("h1", ModHub\ht("a", "hMod Hub", array("href" => "/", "backbone" => true))))
-            ->appendContent(ModHub\ht("h3", "A Great Journey is to be pursued. Greatness Awaits."));
+            ->appendContent(ModHub\ht("h3", "A Great Journey is to be pursued. Greatness Awaits."))
+        ;
 
         return $header_content;
     }
