2abe6de3ee60d7ce9786688513fc88e9c6524b58
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-02 20:59:52 +0100

    Deferring minification step until later, since we *may* debug code in Firebug later on

--- js-compile.bat
+++ js-compile.bat
@@ -4,4 +4,5 @@ set DIRECTORY=%~dp0
 REM Delete old build artifacts, since r.js will have trouble reading old main.js
 del %DIRECTORY%\cache\main.js %DIRECTORY%\cache\main-dist.js
 
-php "%DIRECTORY%\src\AnhNhan\ModHub\cli.php" rsrc:compile && r.js.cmd -o build.js optimize=none && uglifyjs cache/main-dist.js --comments -c -m -o cache/main.js && uglifyjs cache/libs-pck --comments -c -m -o cache/libs-pck
+php "%DIRECTORY%\src\AnhNhan\ModHub\cli.php" rsrc:compile && r.js.cmd -o build.js optimize=none
+REM && uglifyjs cache/main-dist.js --comments -c -m -o cache/main.js && uglifyjs cache/libs-pck --comments -c -m -o cache/libs-pck
