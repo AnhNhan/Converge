4cc42aca2e1715c5e1294097178f7458b4b26f3e
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-02 21:22:11 +0100

    Adding ForumRouter.js

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -5,11 +5,13 @@ define([
     'underscore',
     'backbone',
     'router-front-frontrouter',
+    'router-forum-forumrouter',
     'router-tag-tagrouter',
-], function($, _, Backbone, FrontPageRoter, TagRouter) {
+], function($, _, Backbone, FrontPageRoter, ForumRouter, TagRouter) {
     var initialize = function() {
         // TODO: Write all application routers, and start them here
         FrontPageRoter.initialize();
+        ForumRouter.initialize();
         TagRouter.initialize();
 
         // Kicking off routing
