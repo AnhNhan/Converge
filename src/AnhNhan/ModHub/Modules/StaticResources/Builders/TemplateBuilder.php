fde77df79098a3958b9650b49e42c7e7b1b0b221
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-03 21:52:12 +0100

    Adding missing TemplateBuilder, oops!

---
+++ src/AnhNhan/ModHub/Modules/StaticResources/Builders/TemplateBuilder.php
@@ -0,0 +1,20 @@
+<?php
+namespace AnhNhan\ModHub\Modules\StaticResources\Builders;
+
+/**
+ * Actually just copy-pasting
+ *
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+final class TemplateBuilder
+{
+    public static function buildFile($path)
+    {
+        return file_get_contents($path);
+    }
+
+    public static function buildString($string)
+    {
+        return $string;
+    }
+}
