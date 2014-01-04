0996438690ea278ecffd76f0fd8cdcf04f27e61b
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-04 05:44:04 +0100

    Adapting modals.less

--- src/AnhNhan/ModHub/Modules/Examples/Examples/BootstrapExample.php
+++ src/AnhNhan/ModHub/Modules/Examples/Examples/BootstrapExample.php
@@ -18,7 +18,7 @@ final class BootstrapExample extends AbstractExample
     {
         $container = ModHub\ht("div");
         $container->appendContent(ModHub\safeHtml(<<<EOT
-
+<div class="bs-docs-container">
 
 <div class="progress">
 <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
@@ -89,6 +89,86 @@ final class BootstrapExample extends AbstractExample
     <span class="sr-only">10% Complete (danger)</span>
   </div>
 </div>
+
+<p>Modal in action</p>
+
+    <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
+          </div>
+          <div class="modal-body">
+            <h4>Text in a modal</h4>
+            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+
+            <h4>Popover in a modal</h4>
+            <p>This <a href="#" role="button" class="btn btn-default popover-test" title="A Title" data-content="And here's some amazing content. It's very engaging. right?">button</a> should trigger a popover on click.</p>
+
+            <h4>Tooltips in a modal</h4>
+            <p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> should have tooltips on hover.</p>
+
+            <hr>
+
+            <h4>Overflowing text to show scroll behavior</h4>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+            <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn" data-dismiss="modal">Close</button>
+            <button type="button" class="btn primary">Save changes</button>
+          </div>
+
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
+    <div class="bs-example" style="padding-bottom: 24px;">
+      <button class="btn primary large" data-toggle="modal" data-target="#myModal">
+        Launch demo modal
+      </button>
+    </div><!-- /example -->
+
+
+
+</div>
+
+<script>
+
+// tooltip demo
+$('.tooltip-demo').tooltip({
+  selector: "[data-toggle=tooltip]",
+  container: "body"
+});
+
+$('.tooltip-test').tooltip();
+$('.popover-test').popover();
+
+$('.bs-docs-navbar').tooltip({
+  selector: "a[data-toggle=tooltip]",
+  container: ".bs-docs-navbar .nav"
+});
+
+// popover demo
+$("[data-toggle=popover]")
+  .popover();
+
+
+
+$('.bs-docs-container [href=#]').click(function (e) {
+  e.preventDefault()
+});
+
+</script>
 
 EOT
         ));
