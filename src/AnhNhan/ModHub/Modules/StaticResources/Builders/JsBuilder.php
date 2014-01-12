a7750d37d29b3000002a8c98718009af1bc50ca2
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-12 04:53:27 +0100

    Adding UglifyJs support to JsBuilder

--- src/AnhNhan/ModHub/Modules/StaticResources/Builders/JsBuilder.php
+++ src/AnhNhan/ModHub/Modules/StaticResources/Builders/JsBuilder.php
@@ -6,15 +6,34 @@ namespace AnhNhan\ModHub\Modules\StaticResources\Builders;
  */
 final class JsBuilder
 {
+    const SERVICE_NONE     = "none";
+    const SERVICE_UGLIFYJS = "uglifyjs";
+
+    public static $service = self::SERVICE_NONE;
+
     public static function buildFile($path)
     {
-        // Draft
-        return file_get_contents($path);
+        if (self::$service == self::SERVICE_UGLIFYJS) {
+            // Please put the options after the input files
+            list($output, $err) = execx('uglifyjs %s -c -m', $path);
+            return $output;
+        } else {
+            return file_get_contents($path);
+        }
     }
 
     public static function buildString($string)
     {
-        // Draft
-        return $string;
+        if (self::$service == self::SERVICE_UGLIFYJS) {
+            // Stupid UglifyJs, can't open stdin on Windows...
+            $path = \Filesystem::writeUniqueFile(sys_get_temp_dir(), $string);
+            $output = self::buildFile($path);
+
+            // Remove tmp file
+            \Filesystem::remove($path);
+            return $output;
+        } else {
+            return $string;
+        }
     }
 }
