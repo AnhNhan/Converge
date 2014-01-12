3f2d303e41191373a236bb40c0ba9ead7352387a
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-12 02:18:51 +0100

    Removing buttons in header - they are annoying

--- src/AnhNhan/ModHub/Views/Page/HeaderView.php
+++ src/AnhNhan/ModHub/Views/Page/HeaderView.php
@@ -15,24 +15,6 @@ class HeaderView extends AbstractView
     {
         $header_content = ModHub\ht("div")
             ->addClass("header-content")
-            ->appendContent(
-                ModHub\ht("a", "new here?", array(
-                        "style"       => "margin-right: 50px; margin-top: 0; font-size: 3em !important;",
-                        "href"        => "join",
-                        "data-toggle" => "tooltip",
-                        "title"       => "we'll make it feel like at home :)",
-                    ))
-                    ->addClass("btn btn-large btn-success pull-right header-button")
-            )
-            ->appendContent(
-                ModHub\ht("a", "coming back?", array(
-                        "style"       => "margin-right: 30px; margin-top: 0; font-size: 3em !important;",
-                        "href"        => "login",
-                        "data-toggle" => "tooltip",
-                        "title"       => "welcome back!",
-                    ))
-                    ->addClass("btn btn-large btn-default pull-right header-button")
-            )
             ->appendContent(ModHub\ht("h1", ModHub\ht("a", "hMod Hub", array("href" => "/", "backbone" => true))))
             ->appendContent(ModHub\ht("h3", "A Great Journey is to be pursued. Greatness Awaits."))
         ;
