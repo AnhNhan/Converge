bce064f76654ff2131538c5dc64321053f13af8e
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-14 00:23:19 +0100

    Adding first commit for Vagrant stuff

---
+++ _deploy/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Superfluous? Will be done by PuPHPet afaik.
+
+# apt-get update
+# apt-get install -y apache2
+# rm -rf /var/www
+# ln -fs /vagrant/webroot /var/www
+#
+# apt-get install -y mysql-server php5-cli
