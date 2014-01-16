78b37b004b1b3d2cf6a0e9e17bbef5542d1f986b
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-16 07:59:36 +0100

    Update ConsoleCommand.php

--- src/AnhNhan/ModHub/Console/ConsoleCommand.php
+++ src/AnhNhan/ModHub/Console/ConsoleCommand.php
@@ -12,6 +12,35 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 abstract class ConsoleCommand extends Command
 {
     /**
+     * For certain internal purposes only. Do not use unless you know what you
+     * are doing.
+     *
+     * Retrieves an external application, which will share the same service
+     * container as this console command.
+     *
+     * @return BaseApplication The requested application
+     *
+     * @throws Exception If the app could not be found
+     */
+    final protected function getExternalApplication($internalName)
+    {
+        // Hm, should we replace this with an app list from the container?
+        $classes = SymbolLoader::getInstance()
+            ->getConcreteClassesThatDeriveFromThisOne('AnhNhan\ModHub\Web\Application\BaseApplication');
+        $apps = array();
+        foreach ($classes as $class_name) {
+            $apps[] = new $class_name;
+        }
+        $apps = mpull($apps, null, "getInternalName");
+        $app = idx($apps, $internalName);
+        if (!$app) {
+            throw new \Exception("App '{$internalName}' does not exist!");
+        }
+        $app->setContainer($this->container);
+        return $app;
+    }
+
+    /**
      * @var ContainerInterface
      */
     private $container;
@@ -28,11 +57,17 @@ abstract class ConsoleCommand extends Command
      */
     final public function getService($service)
     {
+        if (!$this->container) {
+            return null;
+        }
         return $this->container->get($service, ContainerInterface::NULL_ON_INVALID_REFERENCE);
     }
 
     final public function getServiceParameter($parameter)
     {
+        if (!$this->container) {
+            return null;
+        }
         return $this->container->getParameter($parameter);
     }
 }
