aa8dc5c2f398a4474c1efad5c65f7b67247eb4f1
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-27 16:08:51 +0100

    Adding plenty of global html functions

--- src/AnhNhan/ModHub/functions_html_global.php
+++ src/AnhNhan/ModHub/functions_html_global.php
@@ -18,3 +18,50 @@ function span($class, $contents)
         ->addClass($class)
     ;
 }
+
+function a($contents = null, $href = null, $backbone = false)
+{
+    return mh\ht('a', $contents, array("backbone" => $backbone, "href" => $href));
+}
+
+function h1($contents, $class = '')
+{
+    return mh\ht('h1', $contents)
+        ->addClass($class)
+    ;
+}
+
+function h2($contents, $class = '')
+{
+    return mh\ht('h2', $contents)
+        ->addClass($class)
+    ;
+}
+
+function h3($contents, $class = '')
+{
+    return mh\ht('h3', $contents)
+        ->addClass($class)
+    ;
+}
+
+function h4($contents, $class = '')
+{
+    return mh\ht('h4', $contents)
+        ->addClass($class)
+    ;
+}
+
+function h5($contents, $class = '')
+{
+    return mh\ht('h5', $contents)
+        ->addClass($class)
+    ;
+}
+
+function h6($contents, $class = '')
+{
+    return mh\ht('h6', $contents)
+        ->addClass($class)
+    ;
+}
