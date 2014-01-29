8b05d18706b197c6fdd15d531810c4767a263934
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-01-29 13:29:44 +0100

    Simplifying `TOCExtractor::extractAndProcessFromHtml`

--- src/AnhNhan/ModHub/Modules/Markup/TOCExtractor.php
+++ src/AnhNhan/ModHub/Modules/Markup/TOCExtractor.php
@@ -41,38 +41,31 @@ final class TOCExtractor
     public function extractAndProcessFromHtml($html)
     {
         $base_hash = hash_hmac("crc32", $html, "key?");
-        $crawler = new Crawler($html);
 
         $r = array();
-        $crawler->filter('h1, h2, h3, h4, h5, h6')->each(function (Crawler $node, $i) use (&$r, $base_hash) {
-            foreach ($node as $key => $n) {
-                $h_hash = hash_hmac("crc32", $base_hash . $i . $key . $n->nodeValue, "key?"); // Add $base_hash, $i and $key to make it mostly-unique
-                $h_id   = sprintf("HEADER-%s-%s", $base_hash, $h_hash);
-                $entry = array(
-                    "type" => $n->nodeName,
-                    "text" => str_replace("\n", " ", $n->nodeValue),
-                    "hash" => $h_id,
-                );
-                $r[] = $entry;
-
-                if ($n instanceof \DOMElement) {
-                    // For now, simply overwrite - our markup shouldn't have id's, anyway
-                    $n->setAttribute("id", $h_id);
-                    //mh_var_dump($n->getAttribute("id"));
-                    $newN = $n->cloneNode(true);
-                    $newN->setAttribute("id", $h_id);
-                    //$node->filter("*")->reduce(function () { return true; });
-                    //$n->insertBefore($newN);
-                }
-            }
-        });
-
-        $this->calculateLevels($r);
 
         $ii = 0;
-        $processed_html = preg_replace_callback('/<(h\\d)>/', function ($matches) use (&$ii, $r) {
-            return sprintf("<%s id=\"%s\">", $matches[1], $r[$ii++]["hash"]);
+        $processed_html = preg_replace_callback('/<(h\\d)>(.*?)<\\/\\1>/', function ($matches) use (&$ii, &$r, $base_hash) {
+            $type = $matches[1];
+            $text = $matches[2];
+            $h_hash = hash_hmac("crc32", $base_hash . $ii . $type . $text, "key?"); // Add stuff to make it mostly-unique
+
+            $entry = array(
+                "type" => $type,
+                "text" => str_replace("\n", " ", $text),
+                "hash" => sprintf("HEADER-%s-%s", $base_hash, $h_hash),
+            );
+            $r[] = $entry;
+
+            return sprintf(
+                "<%s id=\"%s\">%s</%1\$s>",
+                $type,
+                $entry["hash"],
+                $text
+            );
         }, $html);
+
+        $this->calculateLevels($r);
 
         return array($r, $processed_html);
     }
