3653adc422f22d3f4dbb41349d2b4c773382517d
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-02-03 02:55:51 +0100

    Added TagAddedView and TagRemovedView

---
+++ src/AnhNhan/ModHub/Modules/Forum/Views/Display/TagAction.php
@@ -0,0 +1,48 @@
+<?php
+namespace AnhNhan\ModHub\Modules\Forum\Views\Display;
+
+use AnhNhan\ModHub as mh;
+use AnhNhan\ModHub\Modules\Tag\Storage\Tag;
+use AnhNhan\ModHub\Modules\Tag\Views\TagView;
+
+/**
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+abstract class TagAction extends ForumDisplayObject
+{
+    /**
+     * @var TagView[]
+     */
+    protected $tags = array();
+
+    public function addTag(Tag $tag)
+    {
+        $this->addTagLabel($tag->label, $tag->color);
+        return $this;
+    }
+
+    public function addTagView(TagView $tagView)
+    {
+        $this->tags[] = $tagView;
+        return $this;
+    }
+
+    public function addTagLabel($label, $color)
+    {
+        $this->addTagView(new TagView($label, $color));
+        return $this;
+    }
+
+    public function render()
+    {
+        $panel = $this->buildBasicPanel();
+        return $panel;
+    }
+
+    protected function getHeaderText()
+    {
+        return mh\hsprintf("<div><strong>%s</strong> %s a tag: %s</div>", $this->username, $this->getVerb(), mh\safeHtml(implode("", $this->tags)));
+    }
+
+    abstract protected function getVerb();
+}
---
+++ src/AnhNhan/ModHub/Modules/Forum/Views/Display/TagAdd.php
@@ -0,0 +1,13 @@
+<?php
+namespace AnhNhan\ModHub\Modules\Forum\Views\Display;
+
+/**
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+class TagAdd extends TagAction
+{
+    protected function getVerb()
+    {
+        return "added";
+    }
+}
