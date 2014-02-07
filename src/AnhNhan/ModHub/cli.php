52f4ff079ba83d186c41d929f997c2e6255e5613
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-02-07 04:33:41 +0100

    Setting DI containers on CLI commands

--- src/AnhNhan/ModHub/cli.php
+++ src/AnhNhan/ModHub/cli.php
@@ -6,13 +6,15 @@ require_once __DIR__ . "/../../__init__.php";
 use Symfony\Component\Console\Application;
 use AnhNhan\ModHub\Modules\Symbols\SymbolLoader;
 
+$container = \AnhNhan\ModHub\Web\Core::loadSfDIContainer();
+
 $command_classes = SymbolLoader::getInstance()
     ->getConcreteClassesThatDeriveFromThisOne('AnhNhan\ModHub\Console\ConsoleCommand');
 
 $cli_application = new Application("ModHub CLI Interface Manager", "0.0.0.0.1");
 
 foreach ($command_classes as $class_name) {
-    $cli_application->add(new $class_name);
+    $cli_application->add(id(new $class_name)->setContainer($container));
 }
 
 $cli_application->run();
