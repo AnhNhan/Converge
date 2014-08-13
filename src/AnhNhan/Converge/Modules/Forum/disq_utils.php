e7f60d02ad02104008c93f0a32363a161d02106d
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-08-13 17:47:00 +0200

    Slight code deduplication

--- src/AnhNhan/Converge/Modules/Forum/disq_utils.php
+++ src/AnhNhan/Converge/Modules/Forum/disq_utils.php
@@ -1,6 +1,7 @@
 <?php
 
 use AnhNhan\Converge as cv;
+use AnhNhan\Converge\Modules\Forum\Storage\Discussion;
 use AnhNhan\Converge\Modules\Forum\Views\Objects\ForumListing;
 use AnhNhan\Converge\Modules\Forum\Views\Objects\ForumObject;
 use AnhNhan\Converge\Modules\Forum\Views\Objects\PaneledForumListing;
@@ -11,26 +12,7 @@ function render_disq_listing(array $disqs, array $postCounts, $title = null)
     $listing = new ForumListing;
     $listing->setTitle($title);
     foreach ($disqs as $discussion) {
-        $object = new ForumObject;
-        $object
-            ->setHeadline($discussion->label)
-            ->setHeadHref("/disq/" . $discussion->cleanId)
-            ->postCount(idx($postCounts, $discussion->uid)["postcount"]);
-
-        $tags = mpull($discussion->tags->toArray(), "tag");
-        $tags = msort($tags, "label");
-        $tags = array_reverse($tags);
-        $tags = msort($tags, "displayOrder");
-        foreach ($tags as $tag) {
-            if (!empty($tag)) {
-                $object->addTag(new TagView($tag->label, $tag->color));
-            }
-        }
-
-        $object->addDetail($discussion->lastActivity->format("D, d M 'y"));
-        $object->addDetail(cv\ht('strong', link_user($discussion->author)));
-
-        $listing->addObject($object);
+        disq_listing_add_object($listing, $discussion, idx($postCounts, $discussion->uid)["postcount"]);
     }
 
     return $listing;
@@ -47,27 +29,32 @@ function render_disq_paneled_listing(array $disqs, array $postCounts, array $tag
     }
 
     foreach ($disqs as $discussion) {
-        $object = new ForumObject;
-        $object
-            ->setHeadline($discussion->label)
-            ->setHeadHref("/disq/" . $discussion->cleanId)
-            ->postCount(idx($postCounts, $discussion->uid)["postcount"]);
-
-        $tags = mpull($discussion->tags->toArray(), "tag");
-        $tags = msort($tags, "label");
-        $tags = array_reverse($tags);
-        $tags = msort($tags, "displayOrder");
-        foreach ($tags as $tag) {
-            if (!empty($tag)) {
-                $object->addTag(new TagView($tag->label, $tag->color));
-            }
-        }
-
-        $object->addDetail($discussion->lastActivity->format("D, d M 'y"));
-        $object->addDetail(cv\ht('strong', link_user($discussion->author)));
-
-        $panelForumListing->addObject($object);
+        disq_listing_add_object($panelForumListing, $discussion, idx($postCounts, $discussion->uid)["postcount"]);
     }
 
     return $panelForumListing;
+}
+
+function disq_listing_add_object(ForumListing $listing, Discussion $discussion, $postCount)
+{
+    $object = new ForumObject;
+    $object
+        ->setHeadline($discussion->label)
+        ->setHeadHref("/disq/" . $discussion->cleanId)
+        ->postCount($postCount);
+
+    $tags = mpull($discussion->tags->toArray(), "tag");
+    $tags = msort($tags, "label");
+    $tags = array_reverse($tags);
+    $tags = msort($tags, "displayOrder");
+    foreach ($tags as $tag) {
+        if (!empty($tag)) {
+            $object->addTag(new TagView($tag->label, $tag->color));
+        }
+    }
+
+    $object->addDetail($discussion->lastActivity->format("D, d M 'y"));
+    $object->addDetail(cv\ht('strong', link_user($discussion->author)));
+
+    $listing->addObject($object);
 }
