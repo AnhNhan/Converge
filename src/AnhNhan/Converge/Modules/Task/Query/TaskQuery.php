4c484daece32010b559b2b7ecd61c5914a0cb906
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-08-15 04:20:55 +0200

    Adding task query

---
+++ src/AnhNhan/Converge/Modules/Task/Query/TaskQuery.php
@@ -0,0 +1,80 @@
+<?php
+namespace AnhNhan\Converge\Modules\Task\Query;
+
+use AnhNhan\Converge\Storage\Query;
+
+/**
+ * @author Anh Nhan Nguyen <anhnhan@outlook.com>
+ */
+final class TaskQuery extends Query
+{
+    const TASK_ENTITY = 'AnhNhan\Converge\Modules\Task\Storage\Task';
+    const TASK_STATUS_ENTITY = 'AnhNhan\Converge\Modules\Task\Storage\TaskStatus';
+    const TASK_PRIORITY_ENTITY = 'AnhNhan\Converge\Modules\Task\Storage\TaskPriority';
+
+    public function retrieveTasksForUids(array $ids)
+    {
+        $eTask = self::TASK_ENTITY;
+        $queryString = "SELECT t, ts, tp FROM {$eTask} t JOIN t.status ts JOIN t.priority tp WHERE t.uid IN (:task_ids)";
+        $query = $this->em()
+            ->createQuery($queryString)
+            ->setParameters(array("task_ids" => $ids))
+        ;
+        return $query->getResult();
+    }
+
+    public function retrieveTasksForLabels(array $labels)
+    {
+        $eTask = self::TASK_ENTITY;
+        $queryString = "SELECT t, ts, tp FROM {$eTask} t JOIN t.status ts JOIN t.priority tp WHERE t.label IN (:task_labels)";
+        $query = $this->em()
+            ->createQuery($queryString)
+            ->setParameters(array("task_labels" => $labels))
+        ;
+        return $query->getResult();
+    }
+
+    public function retrieveTaskStatusForUids(array $ids)
+    {
+        $eTask = self::TASK_STATUS_ENTITY;
+        $queryString = "SELECT ts FROM {$eTask} ts WHERE ts.uid IN (:task_ids)";
+        $query = $this->em()
+            ->createQuery($queryString)
+            ->setParameters(array("task_ids" => $ids))
+        ;
+        return $query->getResult();
+    }
+
+    public function retrieveTaskStatusForLabels(array $labels)
+    {
+        $eTask = self::TASK_STATUS_ENTITY;
+        $queryString = "SELECT ts FROM {$eTask} ts WHERE ts.label IN (:task_labels)";
+        $query = $this->em()
+            ->createQuery($queryString)
+            ->setParameters(array("task_labels" => $labels))
+        ;
+        return $query->getResult();
+    }
+
+    public function retrieveTaskPriorityForUids(array $ids)
+    {
+        $eTask = self::TASK_ENTITY;
+        $queryString = "SELECT tp FROM {$eTask} tp WHERE tp.uid IN (:task_ids)";
+        $query = $this->em()
+            ->createQuery($queryString)
+            ->setParameters(array("task_ids" => $ids))
+        ;
+        return $query->getResult();
+    }
+
+    public function retrieveTaskPriorityForLabels(array $labels)
+    {
+        $eTask = self::TASK_ENTITY;
+        $queryString = "SELECT tp FROM {$eTask} tp WHERE tp.label IN (:task_labels)";
+        $query = $this->em()
+            ->createQuery($queryString)
+            ->setParameters(array("task_labels" => $labels))
+        ;
+        return $query->getResult();
+    }
+}
