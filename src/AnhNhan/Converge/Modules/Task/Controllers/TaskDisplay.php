559a8573fc0d466aecc61a46de67561c215b6047
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-08-19 01:04:34 +0200

    Adding simple xact display

--- src/AnhNhan/Converge/Modules/Task/Controllers/TaskDisplay.php
+++ src/AnhNhan/Converge/Modules/Task/Controllers/TaskDisplay.php
@@ -7,11 +7,16 @@ use AnhNhan\Converge\Views\Web\Response\ResponseHtml404;
 use AnhNhan\Converge\Web\Application\HtmlPayload;
 use YamwLibs\Libs\Html\Markup\MarkupContainer;
 
+use AnhNhan\Converge\Storage\Transaction\TransactionEntity;
+use AnhNhan\Converge\Modules\Task\Storage\TaskTransaction;
+
 /**
  * @author Anh Nhan Nguyen <anhnhan@outlook.com>
  */
 final class TaskDisplay extends AbstractTaskController
 {
+    const CreatePeriodGraceTime = 60;
+
     public function handle()
     {
         $request = $this->request;
@@ -23,19 +28,54 @@ final class TaskDisplay extends AbstractTaskController
             return id(new ResponseHtml404)->setText('This is not the task you are looking for.');
         }
         $user_query = create_user_query($this->externalApp('user'));
-        fetch_external_authors([$task], $user_query);
-        fetch_external_authors([$task], $user_query, 'assignedId', 'setAssigned', 'assigned');
+        $user_uids = mpull($task->transactions->toArray(), 'actorId');
+        $user_uids[] = $task->authorId;
+        if ($task->assignedId)
+        {
+            $user_uids[] = $task->assignedId;
+        }
+        $user_objects = $user_query->retrieveUsersForUIDs($user_uids);
+
+        $task->setAuthor(idx($user_objects, $task->authorId));
+        $task->setAssigned(idx($user_objects, $task->assignedId));
 
         $container = new MarkupContainer;
         $task_panel = render_task($task);
-        $container->push($task_panel);
-
         if ($task->description)
         {
             $custom_rules = get_custom_markup_rules($this->app->getService('app.list'));
             $desc = MarkupEngine::fastParse($task->description, $custom_rules);
             $task_panel->append(cv\safeHtml($desc));
         }
+        $container->push($task_panel);
+
+        if ($task->transactions->count())
+        {
+            $container->push(h2('Timeline'));
+        }
+
+        foreach ($task->transactions as $xact)
+        {
+            if ($xact->type == TransactionEntity::TYPE_CREATE)
+            {
+                continue;
+            }
+
+            if ($xact->createdAt->getTimestamp() < $task->createdAt->getTimestamp() + self::CreatePeriodGraceTime)
+            {
+                continue;
+            }
+
+            $xact_author = idx($user_objects, $xact->actorId);
+            $xact->setAuthor($xact_author);
+            $rendered_xact = render_task_transaction($task, $xact);
+            $container->push($rendered_xact);
+        }
+
+        $this->app->getService('resource_manager')
+            ->requireCss('application-diff')
+            ->requireCss('application-task-display')
+        ;
 
         $payload = new HtmlPayload;
         $payload->setTitle('Task ' . $task->label);
--- src/AnhNhan/Converge/Modules/Task/Views/task_view_utils.php
+++ src/AnhNhan/Converge/Modules/Task/Views/task_view_utils.php
@@ -4,9 +4,13 @@ use AnhNhan\Converge as cv;
 use AnhNhan\Converge\Modules\Task\Storage\Task;
 use AnhNhan\Converge\Modules\Task\Storage\TaskStatus;
 use AnhNhan\Converge\Modules\Task\Storage\TaskPriority;
+use AnhNhan\Converge\Modules\Task\Storage\TaskTransaction;
 use AnhNhan\Converge\Views\Objects\Listing;
 use AnhNhan\Converge\Views\Objects\Object;
 use AnhNhan\Converge\Views\Property\PropertyList;
+
+use Diff as DiffEngine;
+use AnhNhan\Converge\Modules\Markup\Diff\Renderer\Inline as InlineDiffRenderer;
 
 function render_task_listing(array $tasks, $title = null, $empty_message = 'No tasks available')
 {
@@ -58,4 +62,75 @@ function render_task(Task $task)
     ;
 
     return $panel;
+}
+
+function render_task_transaction(Task $task, TaskTransaction $xact, $other = null)
+{
+    $type_label = task_xact_type_label($task, $xact, $other);
+    $header = cv\hsprintf(
+        '<div class="pull-right">%s</div><img src="%s" class="user-profile-image" /><div><strong>%s</strong> <span class="minor-stuff">%s</span></div>',
+        $xact->createdAt->format("D, d M 'y"),
+        $xact->actor->getGravatarImagePath,
+        link_user($xact->actor),
+        $type_label
+    );
+    $panel = panel($header, 'task-panel-xact');
+
+    $panel_body = task_xact_type_body($task, $xact, $other);
+    if ($panel_body)
+    {
+        $panel->append($panel_body);
+    }
+
+    $panel_class = task_xact_type_class($task, $xact, $other);
+    if ($panel_class)
+    {
+        $panel->addClass($panel_class);
+    }
+
+    return $panel;
+}
+
+function task_xact_type_label(Task $task, TaskTransaction $xact, $other = null)
+{
+    switch ($xact->type)
+    {
+        case TaskTransaction::TYPE_EDIT_LABEL:
+            return 'changed the label';
+        case TaskTransaction::TYPE_EDIT_DESC:
+            return 'changed the description';
+        case TaskTransaction::TYPE_EDIT_STATUS:
+            return cv\hsprintf('changed the status from <em>%s</em> to <em>%s</em>', $xact->oldValue, $xact->newValue);
+        case TaskTransaction::TYPE_EDIT_PRIORITY:
+            return cv\hsprintf('changed the priority from <em>%s</em> to <em>%s</em>', $xact->oldValue, $xact->newValue);
+        case TaskTransaction::TYPE_EDIT_ASSIGN:
+            return cv\hsprintf('reassigned the task to <em>%s</em>', $xact->newValue);
+        default:
+            return 'did something';
+    }
+}
+
+function task_xact_type_body(Task $task, TaskTransaction $xact, $other = null)
+{
+    switch ($xact->type)
+    {
+        case TaskTransaction::TYPE_EDIT_LABEL:
+        case TaskTransaction::TYPE_EDIT_DESC:
+            $diff = new DiffEngine([$xact->oldValue], [$xact->newValue], []);
+            return cv\safeHtml($diff->render(new InlineDiffRenderer));
+        default:
+            return null;
+    }
+}
+
+function task_xact_type_class(Task $task, TaskTransaction $xact, $other = null)
+{
+    switch ($xact->type)
+    {
+        case TaskTransaction::TYPE_EDIT_LABEL:
+        case TaskTransaction::TYPE_EDIT_DESC:
+            return 'task-panel-xact-diff';
+        default:
+            return null;
+    }
 }
