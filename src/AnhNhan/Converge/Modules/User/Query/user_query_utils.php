d7a49cdf5de728ce2d8f70a5aa1a39eb1004452e
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-08-19 23:36:43 +0200

    Small optimization

--- src/AnhNhan/Converge/Modules/User/Query/user_query_utils.php
+++ src/AnhNhan/Converge/Modules/User/Query/user_query_utils.php
@@ -9,6 +9,10 @@ function create_user_query($app_or_em)
 
 function fetch_external_authors(array $stuff, UserQuery $query, $id_field = 'authorId', $set_method = 'setAuthor', $author_field = 'author')
 {
+    if (empty($stuff))
+    {
+        return;
+    }
     if (count(array_filter(mpull($stuff, $author_field))) == count($stuff))
     {
         return;
