008dabc8b1ccdd77fc1d5a208fa0f84ffdb05b9c
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-08-19 16:21:04 +0200

    Adding even more generic pull and group functions

--- src/AnhNhan/Converge/functions_very_global.php
+++ src/AnhNhan/Converge/functions_very_global.php
@@ -6,3 +6,31 @@ function to_canonical($name)
     // towards the alphanumerical characters.
     return strtolower(preg_replace('/[\\W_]/', '', $name));
 }
+
+function group($list, callable $predicate)
+{
+    $map = pull($list, $predicate);
+
+    $groups = [];
+    // Pre-allocate groups
+    foreach ($map as $group)
+    {
+        $groups[$group] = [];
+    }
+
+    foreach ($map as $key => $group) {
+        $groups[$group][$key] = $list[$key];
+    }
+
+    return $groups;
+}
+
+function pull($list, callable $value_predicate)
+{
+    $result = [];
+    foreach ($list as $key => $value)
+    {
+        $result[$key] = $value_predicate($value);
+    }
+    return $result;
+}
