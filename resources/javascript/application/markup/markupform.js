27d031cf1f9b1e5686ec371b306f1e7c062f4c9a
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-09-07 03:17:30 +0200

    Supporting draft saving in application/markup/markupform.js

--- resources/javascript/application/markup/markupform.js
+++ resources/javascript/application/markup/markupform.js
@@ -21,15 +21,20 @@
     MarkupAuthoringForm.defaults = {
         preview_uri:  'markup/process',
         output_selector: '.markup-preview-output',
-        output_template: '<div class="time muted pull-right"></div><div class="text"></div>',
+        output_template: '<div class="muted pull-right" style="text-align: right;"><div class="time"></div><div class="saved"></div></div><div class="text"></div>',
         type: 'POST',
         dataType: 'json',
         bounce_delay: 500
     }
 
     MarkupAuthoringForm.prototype.process = function () {
+        var uri_suffix = ''
+        if (this.options.draftKey)
+        {
+            uri_suffix = '?draft_object_key=' + this.options.draftKey
+        }
         $.ajax({
-            url: this.options.preview_uri,
+            url: this.options.preview_uri + uri_suffix,
             type: this.options.type,
             data: { text: this.$element.val() },
             dataType: this.options.dataType,
@@ -39,6 +44,7 @@
                     if (data.status === 'ok') {
                         this.$output.find('.text').html(data.payloads.contents)
                         this.$output.find('.time').html('Took me ' + data.payloads.time + 'ms!')
+                        this.$output.find('.saved').html(data.payloads.last_saved)
                     } else {
                         console.log("Error while processing markup!")
                     }
