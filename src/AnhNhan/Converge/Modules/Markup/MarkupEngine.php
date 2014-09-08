eaaa008d477e4bffa35dce8a98156216bb67f810
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-09-08 03:20:06 +0200

    Ignore

--- src/AnhNhan/Converge/Modules/Markup/MarkupEngine.php
+++ src/AnhNhan/Converge/Modules/Markup/MarkupEngine.php
@@ -11,11 +11,21 @@ class MarkupEngine
 
     private $custom_rules = [];
 
+    private $block_storage;
+
+    public function __construct()
+    {
+        $this->block_storage = new \PhutilRemarkupBlockStorage;
+    }
+
     public function addInputText($text, $key = "default")
     {
         if (isset($this->inputTexts[$key])) {
             throw new \Exception("Input $key already exists. Can't add it again!");
         }
+
+        // Replace diff html code, store them in block storage
+        $text = $this->fixupDiffInput($text);
 
         // Replace opening braces
         $text = str_replace('<', '&lt;', $text);
@@ -46,6 +56,7 @@ class MarkupEngine
             {
                 $text = $rule->apply($text);
             }
+            $text = $this->fixupDiffOutput($text);
             $text = static::fixupText($text);
             $this->outputText[$key] = $text;
         }
@@ -84,5 +95,21 @@ class MarkupEngine
         $engine = new static;
         $engine->setCustomRules($custom_rules);
         return $engine->addInputText($text)->process()->getOutputText();
+    }
+
+    private function fixupDiffInput($corpus)
+    {
+        return preg_replace_callback('/(<(\\/(span|table|thead|tbody|th|tr|td)|((span|table|thead|tbody|th|tr|td)( class="[\w\s-]+")?))>)/i', [$this, 'fixupDiffInput_callback'], $corpus);
+    }
+
+    private function fixupDiffInput_callback($matches)
+    {
+        $code = $matches[0];
+        return $this->block_storage->store($code);
+    }
+
+    private function fixupDiffOutput($text)
+    {
+        return $this->block_storage->restore($text, true);
     }
 }
