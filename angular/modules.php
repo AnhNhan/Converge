f773f897ebe3169773680e0859016eeff0a73f20
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-12-10 00:43:05 +0100

    Filtering out (for now) unwanted js files from modules.php

--- angular/modules.php
+++ angular/modules.php
@@ -3,7 +3,11 @@ include __DIR__ . '/../vendor/autoload.php';
 
 use YamwLibs\Functions\FileFunc;
 
-$files = FileFunc::recursiveScanForDirectories(__DIR__ . '/modules', '.js');
+$files = FileFunc::recursiveScanForDirectories(__DIR__ . '/modules');
+$files = array_filter($files, function ($file)
+    {
+        return preg_match('/\.js$/i', $file) && !preg_match('/\.min\.js$/i', $file) && !preg_match('/-model\.js$/i', $file);
+    });
 
 header("Content-Type: application/javascript");
 
