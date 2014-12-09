4a35c59a3e69d07ca41811dbb3d833bafddf0f44
Anh Nhan Nguyen <anhnhan@outlook.com>
2014-12-10 00:48:43 +0100

    Removing unnecessary stuff

--- angular/modules/infinitescroll.js
+++ angular/modules/infinitescroll.js
@@ -22,24 +22,14 @@ var strgrsdeds = _.debounce(function (e) {
     // Yes, always re-calc these values
     // The user may have resized windows, we may have added items, etc.
     var $window = $(window);
-    var $body   = $(document.body);
-    var $header = $('#header');
 
     var visibleHeight = $window.innerHeight();
-
     var docHeight = $(document).innerHeight();
-    var scrollHeight = docHeight - visibleHeight;
-    var currentScrollPosition = $window.scrollTop();
 
-    console.log({
-        docHeight: docHeight,
-        visibleHeight: visibleHeight,
-        currentScrollPosition: currentScrollPosition,
-        callbacks: pageBottomCallbacks
-    });
+    var currentScrollPosition = $window.scrollTop();
+    var scrollHeight = docHeight - visibleHeight;
 
     if ((scrollHeight - currentScrollPosition) < 100) {
-        console.log('trigger pageBottomCallbacks: ' + pageBottomCallbacks.length);
         for (var i = pageBottomCallbacks.length - 1; i >= 0; i--) {
             pageBottomCallbacks[i]();
         };
